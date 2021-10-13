40b7223741e85909042000c4efc1ee2932e3860d
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-10-13 20:04:03 +0530

    Creating Table Name:- Employee PayRoll

--- SQLQuery7.sql
+++ SQLQuery7.sql
@@ -1,3 +1,10 @@
 create database payroll_service
-
 use payroll_service
+
+create table employee_payroll
+(
+ id int identity(1,1) primary key,
+ name varchar(100) not null,
+ salary int not null,
+ start_date date not null
+);
